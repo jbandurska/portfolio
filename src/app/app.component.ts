c60cafca6ca47e86ab75efd17c071704900ddc3a
jbandurska <julka.ban@gmail.com>
2024-04-09 12:57:09 +0200

    Languages section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { AboutComponent } from './sections/about/about.component';
 import { ProjectsComponent } from './sections/projects/projects.component';
 import { EducationComponent } from './sections/education/education.component';
+import { LanguagesComponent } from './sections/languages/languages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AboutComponent, ProjectsComponent, EducationComponent],
+  imports: [AboutComponent, ProjectsComponent, EducationComponent, LanguagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/sections/languages/languages.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SvgIconComponent } from '../../shared/components/svg-icon/svg-icon.component';
+
+@Component({
+  selector: 'app-languages',
+  standalone: true,
+  imports: [SvgIconComponent],
+  templateUrl: './languages.component.html',
+  styleUrl: './languages.component.scss'
+})
+export class LanguagesComponent {
+
+}
