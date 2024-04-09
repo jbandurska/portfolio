8a86ff84faef47179efff78fa30841de907ff6b8
jbandurska <julka.ban@gmail.com>
2024-04-09 12:32:49 +0200

    Education section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from './sections/about/about.component';
 import { ProjectsComponent } from './sections/projects/projects.component';
+import { EducationComponent } from './sections/education/education.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AboutComponent, ProjectsComponent],
+  imports: [AboutComponent, ProjectsComponent, EducationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/sections/education/education.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SvgIconComponent } from '../../shared/components/svg-icon/svg-icon.component';
+
+@Component({
+  selector: 'app-education',
+  standalone: true,
+  imports: [SvgIconComponent],
+  templateUrl: './education.component.html',
+  styleUrl: './education.component.scss'
+})
+export class EducationComponent {
+
+}
